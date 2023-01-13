bbffb5be70a38592251dacc5cb5c76818db09e97
Igor.Bonic <igor.bonic.ib@gmail.com>
2023-01-13 20:15:39 +0100

    Changes

--- admin_about.php
+++ admin_about.php
@@ -6,11 +6,11 @@ session_start();
 
 $admin_id = $_SESSION['admin_id'];
 
-if(!isset($admin_id)){
+if (!isset($admin_id)) {
    header('location:login.php');
 };
 
-if(isset($_GET['delete'])){
+if (isset($_GET['delete'])) {
    $delete_id = $_GET['delete'];
    mysqli_query($conn, "DELETE   FROM `reviews` WHERE id = '$delete_id'") or die('query failed');
    header('location:admin_about.php');
@@ -21,6 +21,7 @@ if(isset($_GET['delete'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -34,45 +35,46 @@ if(isset($_GET['delete'])){
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>
+
 <body>
-   
-<?php include 'admin_header.php'; ?>
 
-<section class="messages">
+   <?php include 'admin_header.php'; ?>
 
-   <h1 class="title"> client's reviews </h1>
+   <section class="messages">
 
-   <div class="box-container">
-   <?php
-      // $select_reviews = mysqli_query($conn, "SELECT * FROM `reviews`");
-      $id=$_SESSION['user_id'];
-      $sql=mysqli_query($conn, "SELECT * 
+      <h1 class="title"> client's reviews </h1>
+
+      <div class="box-container">
+         <?php
+         $select_reviews = mysqli_query($conn, "SELECT * FROM `reviews`");
+         $id = $_SESSION['admin_id'];
+         $sql = mysqli_query($conn, "SELECT * 
       FROM `users` 
       JOIN `reviews` ON reviews.fk_user_id=users.id
-      JOIN `orders` ON orders.id=reviews.fk_product_id" );
-      if(mysqli_num_rows($sql) > 0){
-         while($fetch_reviews = mysqli_fetch_assoc($sql)){
-            // var_dump($fetch_reviews);
-   ?>
-      <div class="box">
-         <p><?php echo $fetch_reviews['name']; ?></p>
-         <p><?php echo $fetch_reviews['review']; ?></p>
-         <p>Products ordered:<?php echo $fetch_reviews['total_products']; ?></p>
+      JOIN `orders` ON orders.id=reviews.fk_product_id");
+         if (mysqli_num_rows($sql) > 0) {
+            while ($fetch_reviews = mysqli_fetch_assoc($sql)) {
+               // var_dump($fetch_reviews);
+         ?>
+               <div class="box">
+                  <p><?php echo $fetch_reviews['name']; ?></p>
+                  <p><?php echo $fetch_reviews['review']; ?></p>
+                  <p>Products ordered:<?php echo $fetch_reviews['total_products']; ?></p>
 
-         <a href="admin_about.php?delete=<?php echo $fetch_reviews['id']; ?>" class="delete-btn" onclick="return confirm('delete this review?');">delete</a>
-        
-        
+                  <a href="admin_about.php?delete=<?php echo $fetch_reviews['id']; ?>" class="delete-btn" onclick="return confirm('delete this review?');">Delete</a>
+
+
+               </div>
+         <?php
+            }
+         } else {
+            echo '<p class="empty">No Reviews at the moment !</p>';
+         }
+         ?>
+
       </div>
-<?php
-      }
-   }else{
-      echo '<p class="empty">no review added yet!</p>';
-   }
-   ?>
 
-   </div>
-
-</section>
+   </section>
 
 
 
@@ -80,8 +82,9 @@ if(isset($_GET['delete'])){
 
 
 
-<!-- custom admin js file link  -->
-<script src="js/admin_script.js"></script>
+   <!-- custom admin js file link  -->
+   <script src="js/admin_script.js"></script>
 
 </body>
+
 </html>
